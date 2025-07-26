cd852df260223e604ef50ff6f498c5184fa02a7a
Viktoriia <mwa10@ukr.net>
2025-07-26 03:09:04 +0300

    create project

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAutT17hINeRnXuV41JsmTxyMMTCU8jDN0",
+    authDomain: "nanny-finder-86a1e.firebaseapp.com",
+    projectId: "nanny-finder-86a1e",
+    storageBucket: "nanny-finder-86a1e.firebasestorage.app",
+    messagingSenderId: "128380149641",
+    appId: "1:128380149641:web:0fef423f06a970ac618b58"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
